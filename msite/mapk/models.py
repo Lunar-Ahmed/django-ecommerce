51581fda23d7556381fb4d0026dd5e5eed5012f7
Ahmed <lunitmohammed697@gmail.com>
2024-10-09 19:14:28 -0700

    updates

--- msite/mapk/models.py
+++ msite/mapk/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 class Pdetail(models.Model):
     pname = models.CharField(max_length=200)
+    pprice = models.TextField(max_length=10, null=True)
     psize = models.TextField(max_length=15)
     punit = models.TextField(max_length=50)
     pdescription = models.TextField(max_length=1000)
-
-
-    # pimage = models.ImageField(upload_to='images/')
