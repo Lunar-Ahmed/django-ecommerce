4f9710ca1c0eade5198af4ef14e3c076ced86bcb
Ahmed <lunitmohammed697@gmail.com>
2024-10-09 18:37:22 -0700

    updates

--- msite/mapk/views.py
+++ msite/mapk/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Pdetail
 
 def main(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
 def details(request):
+    pdes = Pdetail.objects.get(id=id)
     template = loader.get_template('details.html')
+    context = {
+        'pdes' : pdes
+    }
     return  HttpResponse(template.render())
