ffb7ceb30d5556e4bf9b6cc76f8c6bf3dbe0f0f7
Ahmed <lunitmohammed697@gmail.com>
2024-10-15 05:15:31 -0700

    updates

--- msite/mapk/models.py
+++ msite/mapk/models.py
@@ -24,8 +24,12 @@ class Register(models.Model):
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class Login(models.Model):
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=250)
+
     
 
