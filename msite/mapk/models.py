ecd3d8f4e1c3a38c2948c2a487cb5a070d67d002
Ahmed <lunitmohammed697@gmail.com>
2024-10-18 00:01:19 -0700

    updates

--- msite/mapk/models.py
+++ msite/mapk/models.py
@@ -5,7 +5,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(default='sneaker1.jpg', blank=True)
+    image = models.ImageField( blank=True)
+    image2 = models.ImageField( blank=True)
+    image3 = models.ImageField( blank=True)
+    image4 = models.ImageField( blank=True)
+    image5 = models.ImageField( blank=True)
+
 
     def __str__(self):
         return self.name
